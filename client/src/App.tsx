502a353446a0a2bffa2a086d670dae84675395ad
Ankush263 <ankushbanik263@gmail.com>
2024-01-12 10:57:02 +0530

    Add loading functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import VideoContainer from './components/videoComponents/VideoContainer';
 import NavBar from './components/nav/NavBar';
 import { getAllVideos } from './api/index';
 import { useEffect, useState } from 'react';
+import { Loader } from '@mantine/core';
 
 interface Videos {
 	_id: string;
@@ -12,9 +13,13 @@ interface Videos {
 
 function App() {
 	const [videos, setVideos] = useState<Videos[]>([]);
+	const [loading, setLoading] = useState<boolean>(false);
+
 	const fetch = async () => {
+		setLoading(true);
 		const doc = await getAllVideos();
 		setVideos(doc.data.data.video);
+		setLoading(false);
 	};
 
 	useEffect(() => {
@@ -25,15 +30,21 @@ function App() {
 		<Flex direction={'column'} justify={'center'} align={'center'}>
 			<NavBar />
 			<Box w={'95%'} mt={20} mb={20}>
-				<Grid>
-					{videos.map((video) => {
-						return (
-							<Grid.Col span={4} key={video._id}>
-								<VideoContainer video={video.video} caption={video.caption} />
-							</Grid.Col>
-						);
-					})}
-				</Grid>
+				{loading ? (
+					<Flex justify={'center'} align={'center'} mih={'80vh'}>
+						<Loader variant="bars" />
+					</Flex>
+				) : (
+					<Grid>
+						{videos.map((video) => {
+							return (
+								<Grid.Col span={4} key={video._id}>
+									<VideoContainer video={video.video} caption={video.caption} />
+								</Grid.Col>
+							);
+						})}
+					</Grid>
+				)}
 			</Box>
 		</Flex>
 	);
